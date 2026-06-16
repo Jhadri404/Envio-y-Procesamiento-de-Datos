3e98f62403b2dc91500ede0b464166125f00d26e
jorge <jorgexss123@gmail.com>
2026-06-16 09:51:24 -0600

    Se aplicaron las variables de entornos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,19 @@
 import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config(); //se puede rellenar con el nombre del .env por ejemplo (".env.db")
+
+const NAME=process.env.SERVER_NAME;
+const VERSION=process.env.SERVER_VERSION;
+const DESCRIPTION=process.env.SERVER_DESCRIPTION;
+const PORT=process.env.SERVER_PORT;
 const app = express();
+
 app.get("/", (req, res) => {
-  res.send("<h1>Server is running</h1>");
+  res.send(`<h1>Nombre del server: ${NAME}</h1><p>${DESCRIPTION}</p><p>Escuchando en el puerto: ${PORT}</p><p>Version: ${VERSION}</p>`);
 });
 
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
