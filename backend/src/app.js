3ae058b26a029e1ca00bc3cafae87eeded9d7a4f
jorge <jorgexss123@gmail.com>
2026-06-16 11:21:12 -0600

    se modifico app.js para poder calcular y validad datos sobre el parqueo

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,15 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config(); //se puede rellenar con el nombre del .env por ejemplo (".env.db")
 
-const NAME=process.env.SERVER_NAME;
-const VERSION=process.env.SERVER_VERSION;
-const DESCRIPTION=process.env.SERVER_DESCRIPTION;
-const PORT=process.env.SERVER_PORT;
+const NAME = process.env.SERVER_NAME;
+const VERSION = process.env.SERVER_VERSION;
+const DESCRIPTION = process.env.SERVER_DESCRIPTION;
+const PORT = process.env.SERVER_PORT;
 
-
- const app = express();
+const app = express();
 
 app.use(cors());
 app.use(express.json());
@@ -20,11 +19,50 @@ app.get("/", (req, res) => {
     name: NAME,
     version: VERSION,
     description: DESCRIPTION,
-    port: PORT
+    port: PORT,
   });
 });
+
+app.post("/api/parqueo/calcular", (req, res) => {
+  const { placa, tipo, horas, minutos } = req.body; //esto desestructura el json, aca tambien puede hacerse de otra manera
+
+if (!placa || placa.trim() === "") {
+   res.status(400).json({ error: "La placa es requerida" });
+}
+if (!tipo || (tipo !== "carro" && tipo !== "moto")) {
+ res.status(400).json({ error: "El tipo de vehículo no es válido" });
+}
+
+if(Number.isNaN(horas) || horas < 0) {
+  res.status(400).json({ error: "Las horas deben ser un valor numérico positivo" });
+}
+
+if(Number.isNaN(minutos) || minutos < 0  || minutos > 59) {
+  res
+    .status(400)
+    .json({ error: "Los minutos deben ser un valor numérico entre 0 y 59" });
+}
+
+const tarifa = tipo === "carro" ? 1200 : 500;
+
+let h=Number(horas)
+let m=Number(minutos)
+
+if (m>5) h++;
+const total = h * tarifa;
+
+res.json({ 
+  placa: placa, 
+  tipo: tipo, 
+  tarifa: tarifa,
+  tiempoUso : horas+":"+minutos,
+  horasCobradas: h, 
+  total });
+  
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
